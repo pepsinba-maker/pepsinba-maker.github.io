d77d20e15e7204f68b0de72e91d13e0ec4c6e610
pepsinba-maker <pepsi.nba@gmail.com>
2025-08-29 19:27:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const app = express();
 app.use(express.json());
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+const serviceAccount = JSON.parse(process.env.GOOGLE_APP);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
